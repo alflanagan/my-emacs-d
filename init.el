6b31be4930b609f590065ea86caf4d4f25f0f984
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2024-02-08 14:05:18 -0500

    some cleanup, but triggered a bug, trying to track it down

--- init.el
+++ init.el
@@ -9,52 +9,118 @@
 
 ;; This file is not part of GNU Emacs.
 
-;; tree-sitter: to install a language use treesit-install-language-grammar. If the language is part of the tree-sitter
-;; project it will fill in the URL for you, just accept the defaults.
-
 
+;;; custom lisp directory
 
 (let ((lispdir (expand-file-name "~/.emacs.d/lisp")))
   (unless (member lispdir load-path)
     (push lispdir load-path)))
 
+;;; NOTE: customizations are in custom.el -- better-defaults changes the destination 
 
-;; the one "better" default I don't like
-(setq visible-bell nil)
+;;; Packages
+;;; Eventual goal is to remove from customization entirely, and use use-package for all.
 
-;; better-defaults set custom-file to custom.el
-(load custom-file)
+(package-initialize)
+(paradox-enable)
 
-
-;; customization took care of the list of packages, so (use-package) is only needed
-;; when you want to set :init, :config, :bind, etc.
+;; KEEP THIS SORTED!
+; except for this, it kind of needs to be first
 (use-package
- package
+ better-defaults
+ :ensure t
  :config
  (progn
-   (package-initialize)
-   (paradox-enable)))
+   ;; the one "better" default I don't like
+   (setq visible-bell nil)
 
-(use-package ace-jump-mode :bind (("C-c SPC" . ace-jump-mode) ("C-x SPC" . ace-jump-mode-pop-mark)))
+   ;; better-defaults set custom-file to custom.el
+   (load custom-file)))
+
+;; (use-package async :ensure t)
+;; (use-package auto-header :ensure t)
+;; (use-package blacken :ensure t)
+;; (use-package cmake-mode :ensure t)
+;; (use-package counsel :ensure t)
+;; (use-package counsel-projectile :ensure t)
+;; (use-package devdocs :ensure t)
+;; (use-package docker-compose-mode :ensure t)
+;; (use-package dockerfile-mode :ensure t)
+;; (use-package dumb-jump :ensure t :config (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
+;; (use-package editorconfig :ensure t :config (editorconfig-mode 1))
+;; (use-package editorconfig-generate :ensure t)
+;; (use-package eldoc :ensure t)
+;; (use-package elisp-autofmt :ensure t :bind (:map emacs-lisp-mode-map (("C-c f" . elisp-autofmt-buffer))))
+;; (use-package elisp-def :ensure t)
+;; (use-package elisp-lint :ensure t)
+;; (use-package elisp-refs :ensure t)
+;; (use-package eslint-disable-rule :ensure t)
+;; (use-package eslint-fix :ensure t)
+;; (use-package flycheck :ensure t)
+;; (use-package flycheck-aspell :ensure t)
+;; (use-package flycheck-bashate :ensure t)
+;; (use-package flycheck-cask :ensure t)
+;; (use-package flycheck-clang-tidy :ensure t)
+;; (use-package flycheck-eglot :ensure t)
+;; (use-package flycheck-golangci-lint :ensure t)
+;; (use-package flycheck-jest :ensure t)
+;; (use-package flycheck-kotlin :ensure t)
+;; (use-package flycheck-mypy :ensure t)
+;; (use-package flycheck-package :ensure t)
+;; (use-package flycheck-pycheckers :ensure t)
+;; (use-package flycheck-relint :ensure t)
+;; (use-package flycheck-rust :ensure t)
+;; (use-package flylisp :ensure t)
+;; (use-package focus-autosave-mode :ensure t)
+;; (use-package fold-dwim :ensure t)
+;; (use-package form-feed-st :ensure t :config (add-hook 'emacs-lisp-mode-hook 'form-feed-st-mode))
+;; (use-package forth-mode :ensure t)
+;; (use-package git-modes :ensure t)
+;; (use-package gnu-elpa-keyring-update :ensure t)
+;; (use-package go :ensure t)
+;; (use-package go-autocomplete :ensure t)
+;; (use-package go-eldoc :ensure t)
+;; (use-package go-mode :ensure t)
+;; (use-package go-projectile :ensure t)
+;; (use-package go-scratch :ensure t)
+;; (use-package guru-mode :ensure t)
+;; (use-package highlight-parentheses :ensure t)
+
+(use-package ibuffer :ensure t :bind (("C-x C-b" . ibuffer-list-buffers)))
+(use-package ivy :ensure t :config (ivy-mode 1))
+(use-package mwim :ensure t :bind (("C-a" . mwim-beginning) ("C-e" . mwim-end)))
+
+(use-package
+ projectile
+ :ensure t
+ :config (projectile-mode +1)
+ :bind (:map projectile-mode-map ("s-p" . projectile-command-map)))
 
 (use-package
  smart-mode-line
+ :ensure t
  :config
  (progn
    (sml/setup)
    (sml/apply-theme 'powerline)))
 
-(use-package ibuffer :bind (("C-x C-b" . ibuffer-list-buffers)))
+(use-package
+ immaterial-theme
+ :ensure t
+ :config
+ (progn
+   (load-theme 'immaterial-dark t)
+   (load-theme 'immaterial-light t)))
+
+
+;;; Everything Else
+
+(unless (default-value 'server-mode)
+  (server-start t))
 
 ;; because I often hit this key by accident and use "C-x C-c" instead anyway
 (keymap-global-unset "s-q" nil)
 (keymap-global-set "C-x C-p" #'(project-list-buffers t))
-
-(use-package
- server
- :config
- (unless (default-value 'server-mode)
-   (server-start)))
 
 (when (equal system-type 'darwin)
   (setq mac-command-modifier 'meta)
@@ -73,49 +139,28 @@
 ;; M-% launches screen capture
 ;; C-M-k brings up calender in MacOS topbar
 ;; C-M-q locks the screen
-(keymap-global-set "C-%" 'query-replace)
-
+;; enable these for all environments so I don't have to remember on non-Macs
 (bind-keys ("C-c C-q" . indent-pp-sexp) ("C-c C-s" . kill-sexp) ("C-%" . query-replace))
 
 ;; steve yegge's replacements for using Meta
 (bind-keys ("C-x C-m" . execute-extended-command) ("C-c C-m" . execute-extended-command))
 
-(use-package projectile :config (projectile-mode +1) :bind (:map projectile-mode-map ("s-p" . projectile-command-map)))
-
-(use-package
- gameoflife
- :config
- (progn
-   (setq gameoflife-screensaver-timeout 300)
-   (gameoflife-screensaver-mode 1)))
-
-(use-package mwim :bind (("C-a" . mwim-beginning) ("C-e" . mwim-end)))
-
-(use-package elisp-autofmt :bind (("C-c f" . elisp-autofmt-buffer)))
-
-(use-package
- immaterial-theme
- :config
- (progn
-   (load-theme 'immaterial-dark t)
-   (load-theme 'immaterial-light t)))
-
-(use-package ivy :config (ivy-mode 1))
-
-(defun typescript-set-up-tree-sitter ()
-  "Sets up Typescript to be found by tree-sitter, because it's directory structure is not the default."
-  (interactive)
-  (add-to-list
-   'treesit-language-source-alist
-   '(typescript . '("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))))
-
-(use-package
- tree-sitter
- :config
- (progn
-   (add-hook 'tree-sitter-after-first-parse-hook #'typescript-set-up-tree-sitter)))
-
-(use-package form-feed-st :config (add-hook 'emacs-lisp-mode-hook 'form-feed-st-mode))
+(setq treesit-language-source-alist
+      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (css "https://github.com/tree-sitter/tree-sitter-css")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (go "https://github.com/tree-sitter/tree-sitter-go")
+        (html "https://github.com/tree-sitter/tree-sitter-html")
+        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+        (json "https://github.com/tree-sitter/tree-sitter-json")
+        (make "https://github.com/alemuller/tree-sitter-make")
+        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (toml "https://github.com/tree-sitter/tree-sitter-toml")
+        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+        (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
 
 (add-hook
  'emacs-lisp-mode-hook
