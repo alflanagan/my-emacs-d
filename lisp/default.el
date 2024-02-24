480b264ef3e1a517a1d1199e77f7a227cc0c080f
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2024-02-24 12:31:11 -0500

    set projectile search paths, python bin

--- lisp/default.el
+++ lisp/default.el
@@ -1,2 +1,5 @@
 ;; this file should have site-specifc customizations, which may override the values set in init.el or custom.el.
 
+(setq projectile-project-search-path
+      '("~/.emacs.d" "~/Devel/Personal" ("~/Devel/Mobelux" . 3) "~/Devel/HackRVA" "~/Devel/ThirdParty"))
+(setq elisp-autofmt-python-bin "/Users/adrianflanagan/.pyenv/versions/3.12.2/bin/python")
