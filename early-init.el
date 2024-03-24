fb0ed9efc8fed15f961e78a81b20d0929effb20b
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2024-03-24 12:38:24 -0400

    set frame params here so they apply to first frame

--- early-init.el
+++ early-init.el
@@ -8,4 +8,9 @@
           (string-join '("/opt/homebrew/opt/gcc/lib/gcc/13"
                          "/opt/homebrew/opt/libgccjit/lib/gcc/13"
                          "/opt/homebrew/Cellar/gcc/13.2.0/lib/gcc/current/gcc/aarch64-apple-darwin23/13")
-                       ":")))
+                       ":"))
+  ;; need to set this in early-init to set for intitial frame
+  (modify-all-frames-parameters
+   '((horizontal-scroll-bars)
+     (vertical-scroll-bars)
+     (font . "-*-FiraCode Nerd Font Mono-regular-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
