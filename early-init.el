1980585aeef62a51e945a22f24a8212ac4efbc8c
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2024-04-19 08:52:39 -0400

    set load-prefer-newer so we load newest init (I hope)

--- early-init.el
+++ early-init.el
@@ -1,8 +1,11 @@
-;; -*- lexical-binding: t -*-
+;;; early-init --- Set config as early as possible -*- lexical-binding: t -*-
+
+;;; Commentary:
 ;; HACK Work around native compilation on macOS failing with 'ld: library not
 ;; found for -lemutls_w'.
 ;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554
 
+;;; Code:
 (when (equal system-type 'darwin)
   (setenv "LIBRARY_PATH"
           (string-join '("/opt/homebrew/opt/gcc/lib/gcc/13"
@@ -11,6 +14,11 @@
                        ":"))
   ;; need to set this in early-init to set for intitial frame
   (modify-all-frames-parameters
-   '((horizontal-scroll-bars)
+   '((horizontal-scroll-bars t)
      (vertical-scroll-bars)
      (font . "-*-FiraCode Nerd Font Mono-regular-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
+
+;; set this here so it gets applied to load of init.el (we hope)
+(setq load-prefer-newer t) ;; IMHO the default value of this being nil is nuts
+
+;;; early-init.el ends here
