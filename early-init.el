209958203212825f95473c8816989246beb5ad25
A. Lloyd Flanagan <lloyd.flanagan@mobelux.com>
2024-04-23 19:20:53 -0400

    set up frames for all environments, except font for Mac

--- early-init.el
+++ early-init.el
@@ -4,6 +4,7 @@
 ;; HACK Work around native compilation on macOS failing with 'ld: library not
 ;; found for -lemutls_w'.
 ;; https://github.com/d12frosted/homebrew-emacs-plus/issues/554
+;; also setup of options which the initial frame shoud obey
 
 ;;; Code:
 (when (equal system-type 'darwin)
@@ -12,16 +13,13 @@
                          "/opt/homebrew/opt/libgccjit/lib/gcc/13"
                          "/opt/homebrew/Cellar/gcc/13.2.0/lib/gcc/current/gcc/aarch64-apple-darwin23/13")
                        ":"))
-  ;; need to set this in early-init to set for intitial frame
   (modify-all-frames-parameters
-   '((horizontal-scroll-bars t)
-     (vertical-scroll-bars)
-     (font . "-*-FiraCode Nerd Font Mono-regular-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
+   '((font . "-*-FiraCode Nerd Font Mono-regular-normal-normal-*-16-*-*-*-m-0-iso10646-1"))))
+
+;; need to set this in early-init to set for intitial frame
+(modify-all-frames-parameters '((horizontal-scroll-bars) (vertical-scroll-bars) (width . 180) (height . 60)))
 
 ;; set this here so it gets applied to load of init.el (we hope)
 (setq load-prefer-newer t) ;; IMHO the default value of this being nil is nuts
-
-;; new frame get these params, including initial frame
-(setq default-frame-alist '((width . 180) (height . 60) (vertical-scroll-bars)))
 
 ;;; early-init.el ends here
