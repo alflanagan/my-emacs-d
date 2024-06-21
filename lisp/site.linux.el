6bef2095d39b3167523b5d056457f7833c5b7f85
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2024-06-20 20:00:00 -0400

    site-specific for my linux box

---
+++ lisp/site.linux.el
@@ -0,0 +1,5 @@
+;; this file should have site-specifc customizations, which may override the values set in init.el or custom.el.
+
+(setq python-interpreter "/home/lloyd/.pyenv/shims/python")
+(setq python-shell-exec-path '("/home/lloyd/.pyenv/shims"))
+(setq python-shell-interpreter "/home/lloyd/.pyenv/shims/python")
