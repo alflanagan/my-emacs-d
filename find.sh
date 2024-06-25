2e1785ca1c08865905e99346f07c06d69fb0e4aa
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2024-06-25 09:04:27 -0400

    add more -prunes

--- find.sh
+++ find.sh
@@ -1,1 +1,9 @@
-find . -name xkcd -prune -o -name backups -prune -o -name eln-cache -prune -o -name .git -prune -o -name elpa -prune -o -type f -print
+find . -name devdocs -prune \
+       -o -name elpy -prune \
+       -o -name .cache -prune \
+       -o -name xkcd -prune \
+       -o -name backups -prune \
+       -o -name eln-cache -prune \
+       -o -name .git -prune \
+       -o -name elpa -prune \
+       -o -type f -print
