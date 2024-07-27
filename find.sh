15ac5a22d267a5797d2ad2a2ceb4263a91a8cd24
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2024-07-27 12:26:06 -0400

    find.sh not needed now, Dockerfile not relevant

--- find.sh
+++
@@ -1,9 +0,0 @@
-find . -name devdocs -prune \
-       -o -name elpy -prune \
-       -o -name .cache -prune \
-       -o -name xkcd -prune \
-       -o -name backups -prune \
-       -o -name eln-cache -prune \
-       -o -name .git -prune \
-       -o -name elpa -prune \
-       -o -type f -print
