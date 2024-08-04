2adf2a8cbf441f12a2312debab0a2fc2b59a3390
A. Lloyd Flanagan <a.lloyd.flanagan@gmail.com>
2024-08-04 14:09:52 -0400

    add defers, turn off line-number-mode for treemacs, use :custom for treemacs

--- init.el
+++ init.el
@@ -431,90 +431,17 @@
 
 ;; (use-package super-save)
 ;; (use-package term-projectile)
-(use-package terraform-doc)
-(use-package terraform-mode)
+(use-package terraform-doc :defer t)
+(use-package terraform-mode :defer t)
 
 ;; Treemacs https://github.com/Alexander-Miller/treemacs?tab=readme-ov-file#installation
 (use-package
  treemacs
- :defer t
- ;; :init
- ;; (with-eval-after-load 'winum
- ;;   (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
- ;; :config
- ;; settings with default values
- ;;    treemacs-collapse-dirs
- ;;    (if treemacs-python-executable
- ;;        3
- ;;      0)
- ;;    treemacs-deferred-git-apply-delay 0.5
- ;;    treemacs-directory-name-transformer #'identity
- ;;    treemacs-display-in-side-window t
- ;;    treemacs-eldoc-display 'simple
- ;;    treemacs-file-event-delay 2000
- ;;    treemacs-file-extension-regex treemacs-last-period-regex-value
- ;;    treemacs-file-follow-delay 0.2
- ;;    treemacs-file-name-transformer #'identity
- ;;    treemacs-follow-after-init t
- ;;    treemacs-expand-after-init t
- ;;    treemacs-find-workspace-method 'find-for-file-or-pick-first
- ;;    treemacs-git-command-pipe ""
- ;;    treemacs-goto-tag-strategy 'refetch-index
- ;;    treemacs-header-scroll-indicators '(nil . "^^^^^^")
- ;;    treemacs-hide-dot-git-directory t
- ;;    treemacs-indentation 2
- ;;    treemacs-indentation-string " "
- ;;    treemacs-is-never-other-window nil
- ;;    treemacs-max-git-entries 5000
- ;;    treemacs-missing-project-action 'ask
- ;;    treemacs-move-files-by-mouse-dragging t
- ;;    treemacs-move-forward-on-expand nil
- ;;    treemacs-no-png-images nil
- ;;    treemacs-no-delete-other-windows t
- ;;    treemacs-project-follow-cleanup nil
- ;;    treemacs-persist-file (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
- ;;    treemacs-position 'left
- ;;    treemacs-read-string-input 'from-child-frame
- ;;    treemacs-recenter-distance 0.1
- ;;    treemacs-recenter-after-file-follow nil
- ;;    treemacs-recenter-after-tag-follow nil
- ;;    treemacs-recenter-after-project-jump 'always
- ;;    treemacs-recenter-after-project-expand 'on-distance
- ;;    treemacs-litter-directories '("/node_modules" "/.venv" "/.cask")
- ;;    treemacs-project-follow-into-home nil
- ;;    treemacs-show-cursor nil
- ;;    treemacs-show-hidden-files t
- ;;    treemacs-silent-filewatch nil
- ;;    treemacs-silent-refresh nil
- ;;    treemacs-sorting 'alphabetic-asc
- ;;    treemacs-select-when-already-in-treemacs 'move-back
- ;;    treemacs-space-between-root-nodes t
- ;;    treemacs-tag-follow-cleanup t
- ;;    treemacs-tag-follow-delay 1.5
- ;;    treemacs-text-scale nil
- ;;    treemacs-user-mode-line-format nil
- ;;    treemacs-user-header-line-format nil
- ;;    treemacs-wide-toggle-width 70
- ;;    treemacs-width 35
- ;;    treemacs-width-increment 1
- ;;    treemacs-width-is-initially-locked t
- ;;    treemacs-workspace-switch-cleanup nil)
- ;;
- ;; The default width and height of the icons is 22 pixels. If you are
- ;; using a Hi-DPI display, uncomment this to double the icon size.
- ;;(treemacs-resize-icons 44)
-
- ;;   (treemacs-follow-mode t)
- ;;   (treemacs-filewatch-mode t)
- ;;   (treemacs-fringe-indicator-mode 'always)
- ;;   (when treemacs-python-executable
- ;;     (treemacs-git-commit-diff-mode t))
- ;;
- ;;   (pcase (cons (not (null (executable-find "git"))) (not (null treemacs-python-executable)))
- ;;     (`(t . t) (treemacs-git-mode 'deferred))
- ;;     (`(t . _) (treemacs-git-mode 'simple)))
- ;;
- ;;   (treemacs-hide-gitignored-files-mode nil))
+ :config (line-number-mode 0)
+ :custom
+ (treemacs-is-never-other-window t)
+ (treemacs-sorting 'alphabetic-case-insensitive-asc)
+ (treemacs-width-is-initially-locked t)
  :bind
  ;; some of these replace functions from tab-bar, which I don't use anyway
  (:map
@@ -642,6 +569,7 @@
 (push '("\\.yml\\'" . yaml-ts-mode) auto-mode-alist)
 
 ;; not having a lot of luck setting up emacs as a brew service, so far
+;; and this doesn't seem to work either.
 (use-package
  server
  :config
