7e12d04b32f800e38cf914f9040379ab2f0aa8ce
A. Lloyd Flanagan (he/him) <lloyd.flanagan@proton.me>
2026-04-13 18:35:27 -0400

    Enable Elpaca and configure.

--- init.el
+++ init.el
@@ -22,39 +22,126 @@
 ;;; Code:
 
 
-;;; Set up `use-package' package manager.
+;;; Set up Elpaca package manager.
+
+;; [Elpaca](https://github.com/progfolio/elpaca) is a replacement for the standard
+;; Emacs package manger ~package.el~. It has many good features, but I mostly
+;; wanted package upgrades and installs to occur in a background process.
+
+(setopt
+ package-archives
+ '(("gnu" . "https://elpa.gnu.org/packages/") ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+   ("melpa" . "https://melpa.org/packages/")))
+
+(defvar elpaca-installer-version 0.12)
+(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
+(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
+(defvar elpaca-order
+  '(elpaca :repo "https://github.com/progfolio/elpaca.git"
+           :ref nil :depth 1 :inherit ignore
+           :files (:defaults "elpaca-test.el" (:exclude "extensions"))
+           :build (:not elpaca-activate)))
+(let* ((repo (expand-file-name "elpaca/" elpaca-sources-directory))
+       (build (expand-file-name "elpaca/" elpaca-builds-directory))
+       (order (cdr elpaca-order))
+       (default-directory repo))
+  (add-to-list
+   'load-path
+   (if (file-exists-p build)
+       build
+     repo))
+  (unless (file-exists-p repo)
+    (make-directory repo t)
+    (when (<= emacs-major-version 28)
+      (require 'subr-x))
+    (condition-case-unless-debug err
+        (if-let* ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
+                  ((zerop
+                    (apply #'call-process
+                           `("git" nil ,buffer t "clone" ,@
+                             (when-let* ((depth (plist-get order :depth)))
+                               (list
+                                (format "--depth=%d" depth)
+                                "--no-single-branch"))
+                             ,(plist-get order :repo) ,repo))))
+                  ((zerop
+                    (call-process "git"
+                                  nil
+                                  buffer
+                                  t
+                                  "checkout"
+                                  (or (plist-get order :ref) "--"))))
+                  (emacs (concat invocation-directory invocation-name))
+                  ((zerop
+                    (call-process emacs
+                                  nil
+                                  buffer
+                                  nil
+                                  "-Q"
+                                  "-L"
+                                  "."
+                                  "--batch"
+                                  "--eval"
+                                  "(byte-recompile-directory \".\" 0 'force)")))
+                  ((require 'elpaca))
+                  ((elpaca-generate-autoloads "elpaca" repo)))
+            (progn
+              (message "%s" (buffer-string))
+              (kill-buffer buffer))
+          (error "%s"
+                 (with-current-buffer buffer
+                   (buffer-string))))
+      ((error)
+       (warn "%s" err)
+       (delete-directory repo 'recursive))))
+  (unless (require 'elpaca-autoloads nil t)
+    (require 'elpaca)
+    (elpaca-generate-autoloads "elpaca" repo)
+    (let ((load-source-file-function nil))
+      (load "./elpaca-autoloads"))))
+(add-hook 'after-init-hook #'elpaca-process-queues)
+(elpaca `(,@elpaca-order))
+
+;; And we need to install Elpaca's own version of the ~use-package~ macro.
+
+;; Install use-package support
+(elpaca
+    elpaca-use-package
+  ;; Enable use-package :ensure support for Elpaca.
+  (elpaca-use-package-mode))
 
 ;; first we add melpa archive (use with caution!)
-(setq package-archives
-      '(("gnu" . "https://elpa.gnu.org/packages/")
-        ("nongnu" . "https://elpa.nongnu.org/nongnu/")
-        ("melpa" . "https://melpa.org/packages/")))
+;; (setq package-archives
+;;       '(("gnu" . "https://elpa.gnu.org/packages/")
+;;         ("nongnu" . "https://elpa.nongnu.org/nongnu/")
+;;         ("melpa" . "https://melpa.org/packages/")))
 
 ;;; Bootstrap `use-package'
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
 
-(eval-when-compile
-  (require 'use-package)
-  ;; so I don't have to specify :ensure t on every call
-  (require 'use-package-ensure))
+;; (eval-when-compile
+;;   (require 'use-package)
+;;   ;; so I don't have to specify :ensure t on every call
+;;   (require 'use-package-ensure))
 
-(setopt use-package-always-ensure t)
+;; (setopt use-package-always-ensure t)
 
 ;; drop into error trace if use-package errors
-(when init-file-debug
-  (setq
-   use-package-verbose t
-   use-package-expand-minimally nil
-   use-package-compute-statistics t
-   debug-on-error t))
+;; (when init-file-debug
+;;   (setq
+;;    use-package-verbose t
+;;    use-package-expand-minimally nil
+;;    use-package-compute-statistics t
+;;    debug-on-error t))
 
-(package-initialize)
+;; (package-initialize)
 
 
 
-;;; (Try to) Ensure the latest org-mode is installed
+;; ;;; (Try to) Ensure the latest org-mode is installed
 (use-package
  org
  :pin "gnu"
