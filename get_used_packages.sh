d2e1b1b115c24e79d257272b0a29500a67ec76ed
A. Lloyd Flanagan (he/him) <lloyd.flanagan@proton.me>
2026-07-15 12:25:50 -0400

    Remove shell scripts for use-package checks (replaced by lisp functions).

--- get_used_packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# TODO: What we need to do is extract only the use-package s-expressions
-#       from config.org
-for PKG in $(cat packages.txt )
-do
-  grep ${PKG} config.org
-done | grep use-package | grep -v '^ *;;' | sed -e "s/^ *//" | cut -d' ' -f2 | cut -d')' -f1 | sort -u
